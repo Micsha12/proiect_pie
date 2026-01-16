eb23cf0be7945abba3db92b6c5702c2f21420b6a
Raul <raul.micsa12@yahoo.com>
2026-01-16 14:35:00 +0200

    ffs

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,28 @@
 package com.example;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
+    public static void main(String[] args) {
+        try {
+            // Crearea rotilor cu raze diferite
+            Roata r1 = new Roata(2);
+            Roata r2 = new Roata(3);
+            Roata r3 = new Roata(4);
+            Roata r4 = new Roata(5);
+
+            // Crearea unei masini
+            Masina masina = new Masina("Dacia");
+
+            // Adaugarea rotilor in colectie
+            masina.adaugaRoata(r1);
+            masina.adaugaRoata(r2);
+            masina.adaugaRoata(r3);
+            masina.adaugaRoata(r4);
+
+            // Afisarea ariei totale
+            System.out.println("Aria totala a rotilor este: " + masina.calculeazaArieTotala());
+
+        } catch (PreaMulteRoti e) {
+            System.out.println("Eroare: " + e.getMessage());
+        }
+    }
 }
